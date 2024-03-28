fbcf93efdb3de45f05640d9d54618fa754891e06
Sabrina A <sabrinaangelle@gmail.com>
2024-03-28 18:08:55 +0900

    updated js file

--- game.js
+++ game.js
@@ -1,16 +1,81 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-let randomChosenColor = buttonColors[Math.floor(Math.random() * buttonColors.length)];
 const gamePattern = [];
-const userClickedPattern = [];
+let userClickedPattern = [];
 let level = 0;
 let started = false;
 
 
+// Create an event listener that starts the game upon the first keyboard key being pressed.
+
+$(document).one('keypress', function(){
+
+    if(!started) {
+
+         // Change the main heading to reveal the current game level.
+         $('h1').text(`Level ${level} `);
+
+        // Activate sequence.
+        nextSequence();
+
+        // Start game.
+        started = true;
+
+    }
+
+});
+
+
+// Add an event listener to the buttons.
+
+$('.btn').on('click', function(){
+
+    // Store selected object in variable.
+    let userChosenColor = this.id;
+
+    // Add to the userClickedPattern array.
+    userClickedPattern.push(userChosenColor);
+
+    // Call playSound function.
+    playSound(userChosenColor);
+
+    // Animate element selected by user.
+    animatePress(userChosenColor);
+
+    // Call function to analyze user's selection
+    checkAnswer(userClickedPattern.length - 1);
+
+
+})
+
+// Create a function that checks the user's answer against the computer's sequence.
+
+checkAnswer = currentLevel => {
+
+
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+        console.log('Sucess');
+
+        if (userClickedPattern.length === gamePattern.length) {
+
+            setTimeout(function(){ nextSequence();
+            }, 1000);
+        }
+
+    } else {
+
+        console.log('Wrong');
+    }
+
+}
 
 
 // Create a function that will determine the pattern sequence user must follow.
 
 nextSequence = () => {
+
+    // Once function is called, generate an empty user array.
+    userClickedPattern = [];
 
     // Increase the level by 1 every time function is called.
     level++;
@@ -21,6 +86,9 @@ nextSequence = () => {
     // Create a variable that generates a random number from 0- 3.
     const randomNumber = Math.floor(Math.random() * 4);
 
+    // Pick random index from buttonColors array.     
+    let randomChosenColor = buttonColors[randomNumber];
+ 
     // Push the item to the gamePattern array.
     gamePattern.push(randomChosenColor);
 
@@ -33,20 +101,6 @@ nextSequence = () => {
 }
 
 
-// Create a function that checks the user's answer against the computer's sequence.
-
-checkAnswer = currentLevel => {
-
-
-    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-
-        console.log('Sucess');
-
-    } else {
-        console.log('Wrong');
-    }
-
-}
 
 
 // Create a function that will play a sound corresponding to the color selected.
@@ -82,38 +136,6 @@ playSound = (name) => {
 
 // Create a function that will animate when a color is chosen.
 
-
-animatePress = (currentColor) => {
-
-    currentColor.addClass('pressed');
-}
-
-
-// Add an event listener to the buttons.
-
-$('.btn').on('click', function(){
-
-    // Store selected object in variable.
-    let userChosenColor = this.id;
-
-    // Add to the userClickedPattern array.
-    userClickedPattern.push(userChosenColor);
-
-    // Call playSound function.
-    playSound(userChosenColor);
-
-    // Animate element selected by user.
-    animatePress(userChosenColor);
-
-    // Call function to analyze user's selection
-    checkAnswer(userClickedPattern.length - 1)
-
-
-})
-
-
-// Create a function that adds class list to clicked button.
-
 animatePress = (currentColor) => {
 
     // Add 'pressed' class to buttons that are clicked.
@@ -121,26 +143,9 @@ animatePress = (currentColor) => {
 
     // Remove 'pressed' class 100 miliseconds after button is clicked.
     setTimeout(function() {
+
     $(`#${currentColor}`).removeClass("pressed");    
     }, 100);
 
     }
 
-// Create an event listener that starts the game upon the first keyboard key being pressed.
-
-$(document).one('keypress', function(){
-
-    if(!started) {
-
-         // Change the main heading to reveal the current game level.
-         $('h1').text(`Level ${level} `);
-
-        // Activate sequence.
-        nextSequence();
-
-        // Start game.
-        started = true;
-
-    }
-
-});
